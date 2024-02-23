98cabedcbac91b215419b659428643b58074012a
Abhilash Sreenivasa <abhilash.sreenivasa@gmail.com>
2024-02-23 09:11:41 -0800

    Code cleanup, close (#5)

--- CodeExecutor/index.ts
+++ CodeExecutor/index.ts
@@ -4,7 +4,7 @@ import fs from 'fs'
 import {promisify} from 'util';
 import { exec } from 'child_process';
 
-const PORT:Number=3002
+const PORT:Number=3003
 const app:Express=express() 
 const writeAsync=promisify(fs.writeFile)
 
@@ -28,7 +28,6 @@ app.post('/code',async (req: Request, res:Response)=>{
                 res.status(500).send('Error executing command');
                 return;
             }
-    
             if (stderr) {
                 console.error(`Command stderr: ${stderr}`);
                 res.status(500).send('Error executing command');
@@ -39,13 +38,10 @@ app.post('/code',async (req: Request, res:Response)=>{
         });
 
     } catch(e){
-    
         console.log(e)
         res.send('Error 500 - Failure to write code to a file')
-    }
-    
+    }  
 })
-
 app.listen(PORT,()=>{
     console.log('Working correctly')
 })
