5da558c175a9a56899b71d0be4283abc1403aaa2
Abhilash Sreenivasa <abhilash.sreenivasa@gmail.com>
2024-02-23 09:13:28 -0800

    Code execution (#7)
    * Added runtime containerization by launching child process and executing docker run (#5)
    
    * Sent the output data back to frontend and added type checks for /code route (#5)
    
    * Code cleanup, close (#5)

--- CodeExecutor/index.ts
+++ CodeExecutor/index.ts
@@ -1,9 +1,13 @@
 import express,{Express,Request,Response} from 'express'
 import bodyParser from 'body-parser'
-import path from 'path'
+import fs from 'fs'
+import {promisify} from 'util';
+import { exec } from 'child_process';
 
-const PORT:Number=3001
+const PORT:Number=3003
 const app:Express=express() 
+const writeAsync=promisify(fs.writeFile)
+
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended:false}))
 
@@ -12,11 +16,32 @@ app.get('/',(req:Request,res:Response)=>{
     res.render('index')
 })
 
-app.post('/code',(req: Request, res:Response)=>{
-    const code=req.body.codeText
-    res.send(code)
-})
+app.post('/code',async (req: Request, res:Response)=>{
+    try{
+        const code: string=req.body.codeText
+        const filepath: string='code_files/code.py'
+        await writeAsync(filepath,code)
+        const command = 'docker run -v "$(pwd)/code_files/code.py":/usr/src/code.py codewitus-python';
+        exec(command, (error:Error | null, stdout:string, stderr:string) => {
+            if (error) {
+                console.error(`Error executing command: ${error}`);
+                res.status(500).send('Error executing command');
+                return;
+            }
+            if (stderr) {
+                console.error(`Command stderr: ${stderr}`);
+                res.status(500).send('Error executing command');
+                return;
+            }
+            console.log(`Command stdout: ${stdout}`);
+            res.render('index', { code:stdout} ); 
+        });
 
+    } catch(e){
+        console.log(e)
+        res.send('Error 500 - Failure to write code to a file')
+    }  
+})
 app.listen(PORT,()=>{
     console.log('Working correctly')
 })
