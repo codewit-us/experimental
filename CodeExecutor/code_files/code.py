77e717c09f37b1aa8ba4f1e92a3f8fb4122a5f4b
Abhilash Sreenivasa <abhilash.sreenivasa@gmail.com>
2024-09-11 22:02:26 -0700

    Added container restrictions details (#13)

--- CodeExecutor/code_files/code.py
+++ CodeExecutor/code_files/code.py
@@ -3,11 +3,9 @@ class GradeBook:
        self.grades=[]
 
    def get_average(self):
-        x=0
-        while x==0:
-            if x==1:
-                break
-        return sum(self.grades)/float(len(self.grades))
+       if not self.grades:
+           return 0
+       return sum(self.grades)/float(len(self.grades))
   
    def add_grade(self,grade):
        self.grades.append(grade)
